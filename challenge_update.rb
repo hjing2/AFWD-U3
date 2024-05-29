d9643af23041f7dfdacf52052a8a5d6c04559484
hjing2 <hjing2@academic.rrc.ca>
2024-05-29 03:17:19 +0000

    second commit

--- challenge_update.rb
+++ challenge_update.rb
@@ -1,7 +1,6 @@
 require_relative 'ar.rb'
 
 # To find out all products with a stock quantity greater than 40.
-
 product_great_quantity = Product.where('stock_quantity > 40')
 
 # Add one to all the stock quantities of these products
